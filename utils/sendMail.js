d3a34c018ae76b8db72ed20bed2d06f8ad94c403
svvprasad1250 <venakataprasad5@gmail.com>
2026-03-17 22:43:26 +0530

    fix nodemailer ipv4 issue

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,22 +1,23 @@
 import nodemailer from "nodemailer";
 
-const sendMail = async(to,subject,htmlContent)=>{
+const sendMail = async (to, subject, htmlContent) => {
     const transporter = nodemailer.createTransport({
-        host:"smtp.gmail.com",
+        host: "smtp.gmail.com",
         port: 587,
-        secure:false,
-        auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS,
+        secure: false,
+        family: 4,   // 👈 force IPv4
+        auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
         }
-    })
+    });
 
     await transporter.sendMail({
-        from: `"QuickPayQR" <${process.env.EMAIL_USER}> `,
+        from: `"QuickPayQR" <${process.env.EMAIL_USER}>`,
         to,
         subject,
-        html:htmlContent
-    })
-}
+        html: htmlContent
+    });
+};
 
 export default sendMail;
