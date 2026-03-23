3a9aa999737f1171b1612146777551f52e59c208
svvprasad1250 <venakataprasad5@gmail.com>
2026-03-23 21:47:24 +0530

    sendgrid mail used

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,14 +3,18 @@ import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMail = async (to, subject, htmlContent) => {
-    const msg = {
+    try {
+        const msg = {
         to,
-        from: "venakataprasad5@gmail.com", // must be verified in SendGrid
+        from: "venakataprasad5@gmail.com",
         subject,
         html: htmlContent,
-    };
+        };
 
-    await sgMail.send(msg);
+        await sgMail.send(msg);
+    } catch (error) {
+        console.error("SendGrid error:", error.response?.body || error.message);
+    }
 };
 
 export default sendMail;
